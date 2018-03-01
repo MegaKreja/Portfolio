0396c46f4556cd75ff6801890489c76fd092ac0a
Stefan Krejovic <kreja95ue@gmail.com>
2018-03-01 20:06:36 +0100

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,53 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-console.log();
+module.exports = (env) => {
+	const isProduction = env === "production";
+	const CSSExtract = new ExtractTextPlugin("styles.css");
 
-module.exports = {
-	entry: "./src/app.js",
-	output: {
-		path: path.join(__dirname, "public"),
-		filename: "bundle.js"
-	},
-	module: {
-		rules: [{
-			loader: "babel-loader",
-			test: /\.js$/,
-			exclude: /node_modules/
-		}, {
-			test: /\.s?css$/,
-			use: [
-				"style-loader",
-				"css-loader",
-				"sass-loader"
-			]
-		}, {
-       test: /\.(woff|woff2|eot|ttf|otf|png|jpg|jpeg|svg)$/,
-       use: [
-         'file-loader'
-       ]
-    }]
-	},
-	devtool: "cheap-module-eval-source-map",
-	devServer: {
-		contentBase: path.join(__dirname, "public")
-	}
+	console.log("env", env);
+	return {
+		entry: "./src/app.js",
+		output: {
+			path: path.join(__dirname, "public"),
+			filename: "bundle.js"
+		},
+		module: {
+			rules: [{
+				loader: "babel-loader",
+				test: /\.js$/,
+				exclude: /node_modules/
+			}, {
+				test: /\.s?css$/,
+				use: CSSExtract.extract({
+					use: [
+						{
+							loader: "css-loader",
+							options: {
+								sourceMap: true
+							}
+						},
+						{
+							loader: "sass-loader",
+							options: {
+								sourceMap: true
+							}
+						}
+					]
+				})
+			}, {
+	       test: /\.(woff|woff2|eot|ttf|otf|png|jpg|jpeg|svg)$/,
+	       use: [
+	         'file-loader'
+	       ]
+	    }]
+		},
+		plugins: [
+			CSSExtract
+		],
+		devtool: isProduction ? "source-map" : "inline-source-map",
+		devServer: {
+			contentBase: path.join(__dirname, "public")
+		}
+	};
 };

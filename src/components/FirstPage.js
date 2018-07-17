e156b24819118dc45c074aeef67a0a26f21f3fd7
Stefan Krejovic <kreja95ue@gmail.com>
2018-07-17 12:56:35 +0200

    correction

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -5,10 +5,10 @@ const FirstPage = (props) => {
 		<div className="container">
 			<div className="firstHalf" id="firstPageFirstHalf">
 				<h1 id="name">Stefan Krejovic</h1>
-				<p id="job">Junior Front-End Web Developer</p>
+				<p id="job">Junior Web Developer</p>
 			</div>
 			<div className="secondHalf" id="firstPageSecondHalf">
-				<p id="description">I'am front-end developer based in Uzice, currently looking for a junior job or internship.
+				<p id="description">I'am web developer based in Uzice, currently looking for a junior job or internship.
 				</p>
 			</div>
 		</div>
